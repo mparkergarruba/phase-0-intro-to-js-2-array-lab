354c77c9083dd31b5ab64b78df89df6ff0791728
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-06-08 20:38:48 -0400

    commit tree 634ff6524834d620a6822e7191095454997c5657

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function() {
+    cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield")
+})
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+} 
+destructivelyAppendCat("Ralph")
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+destructivelyPrependCat("Bob")
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    return [...cats.slice(), name]
+}
+appendCat("Broom")
+function prependCat(name) {
+    return [name, ...cats.slice()]
+}
+prependCat("arnold")
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+}
+function removeFirstCat() {
+    return cats.slice(1, cats.length)
+}
